4c2b5045c68b8d3fe6f626389ae8eb3f84d1718b
Sidraj <siddharth.rajguru2013@gmail.com>
2015-09-07 14:33:59 -0500

    Added Floating iFrames - Modified Menu Buttons for added padding left/right - Modifed Footer (not working properly)

--- Controller/IndexController.js
+++ Controller/IndexController.js
@@ -1,12 +1,20 @@
 function LoadPages(name){
 var proj = "/Pages/projects.html";
 var resume = "/Pages/srajguru_resume_Jul2015_ee_soft1.pdf";
+var home = "/Pages/home.html";
+var contact = "/Pages/contact.html";
 if(name == 'projects'){
 document.getElementById('IndexFrame').setAttribute('src', proj);
-};
+}
 if(name == "resume"){
   document.getElementById('IndexFrame').setAttribute('src', resume);
-};
+}
+if(name == "home"){
+  document.getElementById('IndexFrame').setAttribute('src', home);
+}
+if(name == "contact"){
+  document.getElementById('IndexFrame').setAttribute('src', contact);
+}
 
 return true;
-};
+}
