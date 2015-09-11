79e4bf927b0e51db649ec2609ac710536fa186bc
Sidraj <siddharth.rajguru2013@gmail.com>
2015-09-11 16:42:46 -0500

    Modified Carousel to background color - Added formating for Slide content - Added Internships,Hardwre,Software project pages and buttons

--- Controller/IndexController.js
+++ Controller/IndexController.js
@@ -1,23 +1,25 @@
 function LoadPages(name){
 var proj = "/Pages/projects.html";
 var resume = "/Pages/srajguru_resume_Jul2015_ee_soft1.pdf";
-var internships = "/Pages/internships.html";
+var internships = "/Pages/Internships.html";
 var contact = "/Pages/contact.html";
-if(name == 'projects'){
-document.getElementById('IndexFrame').setAttribute('src', proj);
+var software = "/Pages/SoftProjects.html";
+var hardware = "/Pages/HardProjects.html";
+if(name == 'software'){
+document.getElementById('IndexFrame').setAttribute('src', software);
 }
 if(name == "resume"){
   document.getElementById('IndexFrame').setAttribute('src', resume);
 }
 if(name == "internships"){
-  document.getElementById('IndexFrame').setAttribute('src', home);
+  document.getElementById('IndexFrame').setAttribute('src', internships);
 }
-if(name == "contact"){
-  document.getElementById('IndexFrame').setAttribute('src', contact);
+if(name == "hardware"){
+  document.getElementById('IndexFrame').setAttribute('src', hardware);
 }
 
 return true;
 }
 function scrollWin() {
-    window.scrollTo(0, 750);
+    window.scrollTo(0, 1150);
 }
---
+++ Readme.md
@@ -0,0 +1,25 @@
+** Engineer: Siddraj **
+References: Bootstrap Caurousel; Bootstrap Navbar; CSS3;
+
+The purpose of the website is to have a platform to present my resume in more detailed and elaborate format.
+This website was created with no commercial interests
+
+
+//Requirements:
+NodeJS and Node package management installed
+//To install Node
+$ sudo apt-get install nodejs -g
+
+//To install npm
+$ sudo apt-get install npm
+
+To setup the website:
+
+//Clone repository. Use master for the stable version and develop branch for the latest
+$ git clone <repo name>
+
+// install the dependencies required to start the webserver
+$ npm install
+
+// Start the webserver by default running on port:8080
+$ npm start
