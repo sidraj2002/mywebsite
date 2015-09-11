da6b35b055c36a8b26cf040fd6ceaaab5da210ca
Sidraj <siddharth.rajguru2013@gmail.com>
2015-09-10 23:21:32 -0500

    Added Interships page and edited apearance Black theme - edited home page styles font and buttons - added collapsible toolbar (not working on small screen) - modified iframes to cascade one below other

--- Controller/IndexController.js
+++ Controller/IndexController.js
@@ -1,7 +1,7 @@
 function LoadPages(name){
 var proj = "/Pages/projects.html";
 var resume = "/Pages/srajguru_resume_Jul2015_ee_soft1.pdf";
-var home = "/Pages/home.html";
+var internships = "/Pages/internships.html";
 var contact = "/Pages/contact.html";
 if(name == 'projects'){
 document.getElementById('IndexFrame').setAttribute('src', proj);
@@ -9,7 +9,7 @@ document.getElementById('IndexFrame').setAttribute('src', proj);
 if(name == "resume"){
   document.getElementById('IndexFrame').setAttribute('src', resume);
 }
-if(name == "home"){
+if(name == "internships"){
   document.getElementById('IndexFrame').setAttribute('src', home);
 }
 if(name == "contact"){
