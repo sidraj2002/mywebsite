c5392d4d8ede7ae7730362fb5028e0ab0f249c7d
Sidraj <siddharth.rajguru2013@gmail.com>
2015-09-18 12:06:02 -0500

    Added Website updated information to softprojects.html - Changed default port server2 - Fixed typo in About section

--- server2.js
+++ server2.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname ));
-app.listen(8080, function() {
+app.listen(8002, function() {
    console.log('listening');
  });
