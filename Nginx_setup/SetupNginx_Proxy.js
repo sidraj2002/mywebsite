493153302c7bbf75b99170d646d839e6981df0fc
sid <siddharth.rajguru2013@gmail.com>
2015-09-19 18:48:32 -0500

    Modified ShellJS script - Tested on Ubuntu 14.04

--- Nginx_setup/SetupNginx_Proxy.js
+++ Nginx_setup/SetupNginx_Proxy.js
@@ -2,7 +2,7 @@ var shell = require('shelljs');
 
 shell.echo = 'Installing Nginx';
 shell.exec('sudo apt-get update');
-  if(!test('-d', '/etc/nginx/' )){
+  if(!shell.test('-d', '/etc/nginx/' )){
 
       if(shell.exec('sudo apt-get install nginx').code !== 0){
         shell.echo('Failed to fetch Nginx');
@@ -13,13 +13,13 @@ shell.exec('sudo apt-get update');
 
 shell.exec('sudo service nginx start');
 
-cd('/etc/nginx/sites-available');
+shell.cd('/etc/nginx/sites-available');
 
-  if(test('-f', 'default')){
+  if(shell.test('-f', 'default')){
 
     shell.exec('sudo rm -rf default');
   }
 
-mv(-f, '*/mywebsite/Nginx_setup/default', '/etc/nginx/sites-available');
+shell.exec('sudo cp /home/sid/Documents/mywebsite/Nginx_setup/default /etc/nginx/sites-available/');
 
 shell.exec('sudo service nginx reload');
